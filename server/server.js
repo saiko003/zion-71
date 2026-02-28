8eab7f8ec48ff95644f4295da7b86e5846aa059d
saiko003 <34694288+saiko003@users.noreply.github.com>
2026-03-01 00:11:51 +0100

    server.js

--- server/server.js
+++ server/server.js
@@ -480,11 +480,10 @@ function broadcastState() {
     });
 } // Mbyllja e saktë e broadcastState
 
-// Funksioni profesional për përzierjen e letrave
 function shuffle(array) {
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [array[i], array[j]] = [array[j], array[i]];
     }
     return array;
-} 
+}
